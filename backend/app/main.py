deb476abea3ba2cc04863b53d0b2f55f3cb88559
Sourabh Kumar Das <sourabh.das@klizos.com>
2026-03-24 15:02:41 +0000

    remove unwanted code

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,6 @@ from contextlib import asynccontextmanager
 from app.logger import logger
 from app.middleware.log_middleware import log_request_middleware
 from app.core.redis import redis_manager
-from app.dependencies.redis import get_redis
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -35,5 +33,4 @@ app.middleware("http")(log_request_middleware)
 @app.get("/ping")
 async def ping():
     logger.debug("Ping endpoint called")
-    redis_client = await redis_manager.get_client()
     return {"message": "pong"}
