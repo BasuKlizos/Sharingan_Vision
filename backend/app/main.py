76b33d89f0ad48c4d214849a24f7e3e3bdd4130b
ankana <ankana@klizos.com>
2026-04-27 12:13:14 +0000

    allowed origin 3000

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,7 +44,7 @@ app = FastAPI(
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:3000"], # frontend Origin
+    allow_origins=["http://localhost:3000"], # frontend Origin
     allow_origins=["*"], # frontend Origin
     allow_credentials=True,
     allow_methods=["*"],
